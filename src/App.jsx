04af34b2c25f3a105679d69a28af49f9c49ef31a
Julian Bias <julian_bias@yahoo.com>
2024-09-26 20:52:28 -0500

    added code for selected border

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 
-
-
 // Write your Color component here
-const Color = ({ color, setSelectedColor }) => {
-  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+const Color = ({ color, setSelectedColor, isSelected }) => {
+  return (
+    <div
+      className={`${color} ${isSelected ? "selected" : ""}`}
+      onClick={() => setSelectedColor(color)}
+    >
+      {color}
+    </div>
+  );
 };
 
 const App = () => {
-
   const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
@@ -17,9 +22,21 @@ const App = () => {
         <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-        <Color color="violet" setSelectedColor={setSelectedColor}></Color>
-        <Color color="green" setSelectedColor={setSelectedColor}></Color>
-        <Color color="black" setSelectedColor={setSelectedColor}></Color>
+        <Color
+          color="violet"
+          setSelectedColor={setSelectedColor}
+          isSelected={selectedColor === "violet"}
+        ></Color>
+        <Color
+          color="green"
+          setSelectedColor={setSelectedColor}
+          isSelected={selectedColor === "green"}
+        ></Color>
+        <Color
+          color="black"
+          setSelectedColor={setSelectedColor}
+          isSelected={selectedColor === "black"}
+        ></Color>
       </div>
     </div>
   );
